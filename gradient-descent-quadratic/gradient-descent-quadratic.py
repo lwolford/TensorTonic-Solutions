2b7975788fb6f6721252ccfc43c2e9d185d83170
Logen <43486632+lwolford@users.noreply.github.com>
2026-07-09 13:42:10 -0400

    Solve gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -3,8 +3,25 @@ def gradient_descent_quadratic(a, b, c, x0, lr, steps):
     Return final x after 'steps' iterations.
     """
     # Write code here
-    x = x0
+
+    # This works, but just for fun I want to do this recursively
+    """x = x0
     for i in range(steps):
         deriv = (2 * a * x) + b
         x = x - 0.1 * deriv
-    return x
+    return x"""
+
+    if steps == 0:
+        return x0
+
+    deriv = (2 * a * x0) + b
+    x = x0 - lr * deriv
+
+    return gradient_descent_quadratic(
+        a,
+        b,
+        c,
+        x,
+        lr,
+        steps - 1
+    )
